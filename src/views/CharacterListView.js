af84b95fd37503a1ea2215a7c2d05932fcaf8c22
shanreed <srtech5set@gmail.com>
2019-05-23 08:29:05 -0700

    added mapStateToProps and componentdidmount, characters rendering to the screen

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import {getCharacters} from '../actions';
 import { CharacterList } from "../components";
 // import actions
 
@@ -11,11 +12,13 @@ class CharacterListView extends React.Component {
 
   componentDidMount() {
     // call our action
+    this.props.getCharacters();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      <h1>Getting Starwars Characters</h1>
     }
     return (
       <div className="CharactersList_wrapper">
@@ -27,9 +30,17 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+
+const mapStateToProps = state => {
+  // console.log(state)
+  return {
+    characters: state.charsReducer.characters,
+    error: state.charsReducer.error,
+    fetching: state.charsReducer.fetching
+  };
+
+}
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
+mapStateToProps,
+    {getCharacters}
 )(CharacterListView);
